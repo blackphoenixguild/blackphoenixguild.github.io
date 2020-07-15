05a7bf026f1b964f481ec1e187c5b4e78a9f893e
blackphoenixguild <68347493+blackphoenixguild@users.noreply.github.com>
2020-07-15 20:02:52 +0200

    uploaded stuff
    idk if it will still work

---
+++ script.js
@@ -0,0 +1,18 @@
+let request = new XMLHttpRequest();
+request.open("GET", "http://127.0.0.1:5000/events/0");
+request.send();
+request.onload = () => {
+	if (request.status == 200) {
+		var first = JSON.parse(request.response);
+		console.log(first.dungeon);
+		dungeon_ = document.getElementById("dung");
+		dungeon_.innerHTML = first.dungeon;
+		starter_ = document.getElementById("start");
+		starter_.innerHTML = first.starter;
+		info_ = document.getElementById("inf");
+		info_.innerHTML = first.info;
+		console.log(first)
+	} else {
+		console.log('API offline');
+	}
+}
